af719fb72ffd8aa2d34c511189908fc9a5881ae8
Kate Fields <katrinwater@yandex.ru>
2024-06-02 18:33:11 +0200

    app.js with JSX

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+//декларативный  стиль
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+	//сама анонимная функция  - императивный стиль, но вычисление даты - декларативный, возвращенные теги в виде верстки- декларативный
+
+	let currentYear = new Date().getFullYear();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>{currentYear}</p>
+			</header>
+		</div>
+	);
+};
